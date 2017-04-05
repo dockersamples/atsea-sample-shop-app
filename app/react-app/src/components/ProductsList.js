a6071910cd682809fc1b841e2327452ea51d2b7b
Jessica Valarezo <jgv2108@columbia.edu>
2017-04-05 10:53:04 -0700

    Product grid update. - add Grid Tile - add styles

--- app/react-app/src/components/ProductsList.js
+++ app/react-app/src/components/ProductsList.js
@@ -1,29 +1,39 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import ProductItem from '../components/ProductItem'
 
+import { GridList } from 'material-ui/GridList'
+import './ProductsList.css'
 
-const ProductsList = ({ title, products, addToCart }) => (
-  <div>
-    <h3>{title}</h3>
-    <div>
-      {products.map(product =>
-        <ProductItem
-          key={product.productId}
-          product={product}
-          onAddToCartClicked={() => addToCart(product.productId)} />
-      )}
+export default class ProductsList extends Component {
+
+  render() {
+    const { title, products, addToCart } = this.props
+    return(
+      <div>
+        <div className="productListTitle">{title}</div>
+        <GridList cols={4} padding={42} cellHeight={347}>
+          {products.map(product => (
+            <ProductItem
+              key={product.productId}
+              product={product}
+              onAddToCartClicked={addToCart}
+            />
+          ))}
+        </GridList>
       </div>
-  </div>
-)
+    )
+  }
+}
 
 ProductsList.propTypes = {
   products: PropTypes.arrayOf(PropTypes.shape({
     productId: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
+    description: PropTypes.string,
+    image: PropTypes.string,
   })).isRequired,
   addToCart: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired
 }
 
-export default ProductsList

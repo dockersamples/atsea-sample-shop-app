7f647b4f7df75d38582f978031ac79fad3b711ff
Jessica Valarezo <jgv2108@columbia.edu>
2017-04-05 10:52:43 -0700

    GridTile component.

---
+++ app/react-app/src/components/GridTile/index.js
@@ -0,0 +1,61 @@
+import React, { Component, PropTypes } from 'react'
+
+import FlatButton from 'material-ui/FlatButton'
+import AddIcon from './AddIcon'
+import './styles.css'
+
+export default class GridTile extends Component {
+  addToCart = () => {
+    const { onAddToCartClicked, productId } = this.props
+    onAddToCartClicked(productId)
+  }
+
+  render() {
+    const { price, name, description, image } = this.props
+    const image2 = (
+      <img
+        alt="Logo"
+        src={`data:image/png;base64,${image}`}
+        height="115px"
+        width="126px"
+      />
+    )
+    return (
+      <div className="tile">
+        <div className="tileImage">
+          {image2}
+        </div>
+        <div className="tileTitle">
+          {name}
+        </div>
+        <div className="tileDescription">
+          {description}
+        </div>
+        <div className="titleBottom">
+          <div className="tilePrice">
+            {price}
+          </div>
+          <div className="tileAdd">
+            <FlatButton
+              onClick={this.addToCart}
+              labelStyle={{color: "#099CEC"}}
+              label="Add"
+              labelPosition="before"
+              icon={<AddIcon />}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+GridTile.propTypes = {
+  productId: PropTypes.number,
+  price: PropTypes.string,
+  name: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  onAddToCartClicked: PropTypes.func.isRequired
+}
+

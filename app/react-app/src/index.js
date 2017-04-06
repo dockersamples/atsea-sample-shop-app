9724a2a66663586308e47e344b61537dd2fc223c
Jessica Valarezo <jgv2108@columbia.edu>
2017-04-06 14:09:22 -0700

    Routing works.

--- app/react-app/src/index.js
+++ app/react-app/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { Router, Route, hashHistory } from 'react-router'
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import promiseMiddleware from 'redux-promise-middleware'
 import reducer from './reducers'
 import { getAllProducts, fetchAllItems } from './actions'
 import App from './containers/App'
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import CheckoutContainer from './containers/CheckoutContainer'
+
 
 const middleware = [ 
   thunkMiddleware,
@@ -24,7 +27,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const store = createStore(
   reducer,
-  applyMiddleware(...middleware)
+  applyMiddleware(...middleware),
 )
 
 const muiTheme = getMuiTheme({
@@ -39,7 +42,10 @@ store.dispatch(fetchAllItems())
 render(
   <Provider store={store}>
     <MuiThemeProvider muiTheme={muiTheme}>
-      <App />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}/>
+        <Route path="checkout" component={CheckoutContainer} />
+      </Router>
     </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')

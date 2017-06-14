addb0c85dc808b3a7602805343a1ea29cd6d79a1
spara <spara@github.com>
2017-06-14 11:25:31 -0500

    path/requests are case insensitive

--- app/src/main/java/com/docker/atsea/configuration/WebConfiguration.java
+++ app/src/main/java/com/docker/atsea/configuration/WebConfiguration.java
@@ -1,10 +1,12 @@
 package com.docker.atsea.configuration;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 
 @Component
 @EnableWebMvc
@@ -16,7 +18,14 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 	}
 	
 	@Override
-		public void addViewControllers(ViewControllerRegistry registry) {
-		    registry.addViewController("/").setViewName("redirect:/index.html");
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("redirect:/index.html");
 	}
+
+        @Override
+        public void configurePathMatch(PathMatchConfigurer configurer) {
+        	AntPathMatcher matcher = new AntPathMatcher();
+        	matcher.setCaseSensitive(false);
+       		configurer.setPathMatcher(matcher);
+    	}
 }
